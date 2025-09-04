5c6a1bffbab38abf0040996bba19d4bc898c39e2
notoe480pad-a11y <notoe480pad@gmail.com>
2025-09-04 15:45:47 +0900

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,12 @@
+# sample.py
+
+def hello():
+    print("Hello, GitHub!")
+
+def add(a, b):
+    return a + b
+
+# ファイルを直接実行したときだけ動く
+if __name__ == "__main__":
+    hello()
+    print("2 + 3 =", add(2, 3))
